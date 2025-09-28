ff85a3dff6376189a2eae3d4eb41aa7f3f8f5943
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-28 22:36:06 +0700

    Update luexhub.js

--- api/luexhub.js
+++ api/luexhub.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
 
   if (!isBrowser) {
     // Executor -> redirect script .lua
-    res.writeHead(302, { Location: "https://luexteamscript.vercel.app/luex.lua" });
+    res.writeHead(302, { Location: "https://luexteamscript.vercel.app/luex-bsdfhe483rfyeuiywo83rye8owy38ryeuw38r.lua" });
     res.end();
   } else {
     // Browser -> redirect UI
