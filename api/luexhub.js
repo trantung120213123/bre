fe2a530e7eb5618f33843e268fa14be752b6eaeb
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-29 15:02:53 +0700

    Update luexhub.js

--- api/luexhub.js
+++ api/luexhub.js
@@ -1,3 +1,4 @@
+// luexhub.js
 export default function handler(req, res) {
   const ua = req.headers["user-agent"] || "";
 
@@ -5,9 +6,14 @@ export default function handler(req, res) {
   const isBrowser = /(chrome|safari|firefox|edge|coc_coc_browser|opera|edg)/i.test(ua);
 
   if (!isBrowser) {
-    // Executor -> redirect script .lua
-    res.writeHead(302, { Location: "https://luexteamscript.vercel.app/luex-bsdfhe483rfyeuiywo83rye8owy38ryeuw38r.lua" });
-    res.end();
+    // Check for command line tools like curl, wget, powershell
+    if (/curl|wget|powershell/i.test(ua)) {
+      res.status(403).send("Access Denied");
+    } else {
+      // Executor -> redirect script .lua
+      res.writeHead(302, { Location: "https://luexteamscript.vercel.app/luex-bsdfhe483rfyeuiywo83rye8owy38ryeuw38r.lua" });
+      res.end();
+    }
   } else {
     // Browser -> redirect UI
     res.writeHead(302, { Location: "/luex" });
