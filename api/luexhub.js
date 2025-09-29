21eac0020b62251e7527ecf4dadfbd1b3e1a1a4f
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-29 15:06:50 +0700

    Update luexhub.js

--- api/luexhub.js
+++ api/luexhub.js
@@ -8,7 +8,7 @@ export default function handler(req, res) {
   if (!isBrowser) {
     // Check for command line tools like curl, wget, powershell
     if (/curl|wget|powershell/i.test(ua)) {
-      res.status(403).send("Access Denied");
+      res.status(403).send("Access Denied cướp cướp cái lồn mẹ nhà mày");
     } else {
       // Executor -> redirect script .lua
       res.writeHead(302, { Location: "https://luexteamscript.vercel.app/luex-bsdfhe483rfyeuiywo83rye8owy38ryeuw38r.lua" });
