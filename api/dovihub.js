2f2298548919451b47ec4364d43ecb269c7b4dae
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-29 12:06:07 +0700

    Create dovihub.js

---
+++ api/dovihub.js
@@ -0,0 +1,16 @@
+export default function handler(req, res) {
+  const ua = req.headers["user-agent"] || "";
+
+  // Regex check browser
+  const isBrowser = /(chrome|safari|firefox|edge|coc_coc_browser|opera|edg)/i.test(ua);
+
+  if (!isBrowser) {
+    // Executor -> redirect script .lua
+    res.writeHead(302, { Location: "https://luexteamscript.vercel.app/dovi-hub-dscsdcbsjkcbfcjscbdjscgdjskdfcgjkdxsdcdscds.lua" });
+    res.end();
+  } else {
+    // Browser -> redirect UI
+    res.writeHead(302, { Location: "/dovii" });
+    res.end();
+  }
+}
