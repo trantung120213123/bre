07e14b422f156ad36a484c650cf3258b07620e74
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-09-29 15:06:30 +0700

    Update dovihub.js

--- api/dovihub.js
+++ api/dovihub.js
@@ -8,7 +8,7 @@ export default function handler(req, res) {
   if (!isBrowser) {
     // Check for command line tools like curl, wget, powershell
     if (/curl|wget|powershell/i.test(ua)) {
-      res.status(403).send("Access Denied");
+      res.status(403).send("Access Denied cái lồn mẹ nhà mày thích cướp à");
     } else {
       // Executor -> redirect script .lua
       res.writeHead(302, { Location: "https://luexteamscript.vercel.app/dovi-hub-dscsdcbsjkcbfcjscbdjscgdjskdfcgjkdxsdcdscds.lua" });
