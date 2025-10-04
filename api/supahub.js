bf7fa1a23d1264d1ccf5086b204cdad941a63652
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2025-10-04 19:14:16 +0700

    Create supahub.js

---
+++ api/supahub.js
@@ -0,0 +1,22 @@
+// dovihub.js
+export default function handler(req, res) {
+  const ua = req.headers["user-agent"] || "";
+
+  // Regex check browser
+  const isBrowser = /(chrome|safari|firefox|edge|coc_coc_browser|opera|edg)/i.test(ua);
+
+  if (!isBrowser) {
+    // Check for command line tools like curl, wget, powershell
+    if (/curl|wget|powershell/i.test(ua)) {
+      res.status(403).send("Access Denied cái lồn mẹ nhà mày thích cướp à");
+    } else {
+      // Executor -> redirect script .lua
+      res.writeHead(302, { Location: "https://luexteamscript.vercel.app/supa-tech-dscsdcbsjkcbfcjscbdjscgdjskdfcgjkdxsdcdscds.lua" });
+      res.end();
+    }
+  } else {
+    // Browser -> redirect UI
+    res.writeHead(302, { Location: "/supa" });
+    res.end();
+  }
+}
