437f38d2073c613999f96213d4aeaa70e5e53a54
trantung120213123 <142653654+trantung120213123@users.noreply.github.com>
2026-01-18 00:18:53 +0700

    Update autorejoin.lua

--- public/autorejoin.lua
+++ public/autorejoin.lua
@@ -1,2 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/autorj.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/autocheck.lua"))()
+
+local scripts = {
+    "https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/autorj.lua",
+    "https://raw.githubusercontent.com/trantung120213123/Hackroblox/refs/heads/main/autocheck.lua"
+}
+
+for _, url in ipairs(scripts) do
+    pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+end
